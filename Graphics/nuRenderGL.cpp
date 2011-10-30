7992518c64fdb33136f20ffa3e37abb9408e5d7d
Steve Nus <saint_nus@mac.com>
2011-10-30 21:01:37 +0900

    Adding graphics module.
    GL renderer module.

---
+++ Graphics/nuRenderGL.cpp
@@ -0,0 +1,9 @@
+/*!
+ * \file nuRenderGL.cpp
+ * \brief Renderer.
+ * \author Nus
+ * \date 2011/10/30 17:40
+ */
+
+#include "nuRenderGL.h"
+
---
+++ Graphics/nuRenderGL.h
@@ -0,0 +1,13 @@
+/*!
+ * \file nuRenderGL.h
+ * \brief Renderer.
+ * \author Nus
+ * \date 2011/10/30 17:40
+ */
+
+#ifndef __NURENDERGL_H__
+#define __NURENDERGL_H__
+
+
+
+#endif
