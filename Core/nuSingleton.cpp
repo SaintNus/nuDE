cd97fc0799b483b841885214467af4c1db74129d
Steve Nus <saint_nus@mac.com>
2011-11-12 08:48:45 +0900

    Implementing singleton.

---
+++ Core/nuSingleton.cpp
@@ -0,0 +1,9 @@
+//
+//  nuSingleton.cpp
+//  Core
+//
+//  Created by Nus Steve on 11/11/11.
+//  Copyright (c) 2011年 Capcom Japan Co. Ltd. All rights reserved.
+//
+
+#include <iostream>
---
+++ Core/nuSingleton.h
@@ -0,0 +1,14 @@
+//
+//  nuSingleton.h
+//  Core
+//
+//  Created by Nus Steve on 11/11/11.
+//  Copyright (c) 2011年 Capcom Japan Co. Ltd. All rights reserved.
+//
+
+#ifndef Core_nuSingleton_h
+#define Core_nuSingleton_h
+
+
+
+#endif
